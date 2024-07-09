743be26c227e62c67a300415cd1aeb3db8ffcb8e
Laura Kriszgruber <l.kriszgruber@gmail.com>
2024-07-09 18:08:48 +0200

    Split data

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 
-from preprocessing.preprocessor import DataProcessor
+from preprocessing.df_splitter import DataFrameSplitter
+from preprocessing.clean_data import CleanData
+
 
 
 def main():
@@ -7,15 +9,21 @@ def main():
     Main function to create an instance of DataProcessor and execute the processing steps.
     """
     data_path = "data/final_dataset.json"
-    p = DataProcessor(data_path)
+    zip_path = "data/zipcode-belgium.json"
+    save_path = "data/clean_dataset.csv"
+    
+    p = CleanData(data_path, zip_path, save_path)
     p.process()
+    
+    
+    splitter = DataFrameSplitter()
+    splitter.split_and_save(p.get_data())
 
-
-    p.save()
-
-    print(p.get_data().head())
-    print(p.get_column())
-    print(p.get_summary_stats())
+    # print(p.get_data().head())
+    # print(p.get_column())git 
+    # print(p.get_summary_stats())
+    
+    
 
 if __name__ == "__main__":
     main()
