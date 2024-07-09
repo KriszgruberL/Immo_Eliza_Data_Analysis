93cf045938125ebcfd830b4ca9383a2d723dd0ba
Laura Kriszgruber <l.kriszgruber@gmail.com>
2024-07-09 10:45:27 +0200

    Cleaning

--- preprocessing/cleaning.py
+++ preprocessing/cleaning.py
@@ -1,5 +1,16 @@
-import pandas as pd 
-import numpy as np 
+import pandas as pd
+import numpy as np
+import json
+import os
+print(os.getcwd())
+
 data = pd.read_json("data/final_dataset.json")
+
+
 df = pd.DataFrame(data)
-data_csv = df.to_csv('data/dataset.csv')
+data_csv1 = df.to_csv("data/dataset1.csv")
+
+df.columns
+
+print(df.head())
+print("Done!")
