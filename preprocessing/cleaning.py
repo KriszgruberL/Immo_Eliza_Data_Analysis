c95583614acd8a0654ea4a1eaffc7fbb8099bdbb
Kriszgruber Laura <93479232+KriszgruberL@users.noreply.github.com>
2024-07-10 09:07:27 +0200

    Delete preprocessing/cleaning.py

--- preprocessing/cleaning.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd 
-import numpy as np 
-
-data = pd.read_json("data/final_dataset.json")
-df = pd.DataFrame(data)
-data_csv = df.to_csv('data/dataset.csv')
-
-df.drop(columns=['Url'])
-print(df.head())
